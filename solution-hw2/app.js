0baf4918042c6cefe3f62e44130fa9252dd70a47
alanawu3 <112406499+alanawu3@users.noreply.github.com>
2022-09-15 13:00:13 -0400

    Comments

--- solution-hw2/app.js
+++ solution-hw2/app.js
@@ -1,25 +1,25 @@
 let cartItems = [];//store added-to-cart roll objects in here
-let cartTotals = {
+let cartTotals = {//track #items added to cart & total price
     items: 0,
     priceTotal: 0
 }
 let currRolls = []; //holds instances of each roll in its current state on the product page
 
-const glazeOptions = {
+const glazeOptions = {//price adaptation for each glaze type
     'Keep Original': 0,
     'Sugar Milk': 0,
     'Vanilla Milk': 0.5,
     'Double Chocolate': 0.5
 }
 
-const packSizeOptions = {
+const packSizeOptions = {//price adaptation for pack size changes
     1: 1,
     3: 3,
     6: 5,
     12: 10
 }
 
-const basePrice = [2.49, 3.49, 2.99, 3.49, 3.99, 3.99];
+const basePrice = [2.49, 3.49, 2.99, 3.49, 3.99, 3.99]; //starting price for each roll
 
 class Roll {
     constructor(rollType, price, glazing, packSize){
@@ -28,11 +28,11 @@ class Roll {
         this.glazing = glazing;
         this.packSize = packSize;
 
-        currRolls.push(this);
+        currRolls.push(this); //adds new roll product to existing product choices
     }
 }
 
-function callPopup(btn, rollNum) {
+function callPopup(btn, rollNum) { //updates item total, price total, most recent cart addition info, and displays popup
     selectedRoll = currRolls[rollNum];
     let newCartRoll = JSON.parse(JSON.stringify(selectedRoll)); //makes copy of this roll object
     cartItems.push(newCartRoll); //adds to array of rolls in cart
@@ -55,22 +55,22 @@ function callPopup(btn, rollNum) {
     document.getElementById('new-item-pack').innerHTML = 'Pack of ' + cartItems.slice(-1)[0]['packSize'];
     document.getElementById('new-item-price').innerHTML = 'Price: ' + (cartItems.slice(-1)[0]['price']).toFixed(2);
 
-    popup.classList.add("show");
+    popup.classList.add("show"); //turns on visibility for popup
 }
 
-function hidePopup() {
+function hidePopup() {//turns off popup visibility
     let popup = document.getElementById("popup");
     popup.classList.remove("show");
 }
 
-function popupFxns(btn, rollNum) {
+function popupFxns(btn, rollNum) {//calls popup & timer fxn when add-to-cart is clicked
     callPopup(btn, rollNum);
     setTimeout(function () {
         hidePopup();
     }, 3000);
 }
 
-function updatePackSize(element,rollNum, size) { //when a pack size button is clicked, the css is updated to change color backgrounds & this.price is adjusted
+function updatePackSize(element,rollNum, size) {//updates price, btn displays, and roll object when packSize is changed
     let currRoll = currRolls[rollNum];
     currRoll.packSize = size;
     let buttons = document.querySelectorAll('.pack-size > button');
@@ -84,18 +84,17 @@ function updatePackSize(element,rollNum, size) { //when a pack size button is cl
     document.getElementById(rollID).innerHTML = '$ ' + currRoll.price.toFixed(2);
   }
 
-function updateGlazeOption (rollNum) { //update product price, update this.glazing
+function updateGlazeOption (rollNum) { //updates price & roll object
     let glazeNum = document.getElementById(rollNum + '-glaze').value;
     let currRoll = currRolls[rollNum]; //finds which roll type is currently being altered
     let glazeType = Object.keys(glazeOptions)[glazeNum];
     currRoll.glazing = glazeType;
     currRoll.price = (basePrice[rollNum] + glazeOptions[glazeType]) * currRoll.packSize;
     let rollID =  "price-" + String(rollNum);
-    console.log(rollID);
     document.getElementById(rollID).innerHTML = '$ ' + currRoll.price;
 }
 
-
+//roll products displayed on the page
 let originalRoll = new Roll(
     'Original',
     2.49,
